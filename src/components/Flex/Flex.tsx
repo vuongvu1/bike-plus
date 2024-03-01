af07afe8e23c363a8fa06bbb4abc5ae3c26fe675
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-01 23:38:06 +0100

    feat: add Flex component

---
+++ src/components/Flex/Flex.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties } from "react";
+import styles from "./Flex.module.css";
+
+interface FlexProps {
+  direction?: "row" | "row-reverse" | "column" | "column-reverse";
+  justify?:
+    | "flex-start"
+    | "flex-end"
+    | "center"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+  align?: "stretch" | "flex-start" | "flex-end" | "center" | "baseline";
+  children: React.ReactNode;
+}
+
+export const Flex: React.FC<FlexProps> = ({
+  direction = "row",
+  justify = "flex-start",
+  align = "stretch",
+  children,
+}) => {
+  const style = {
+    "--flex-direction": direction,
+    "--flex-justify": justify,
+    "--flex-align": align,
+  } as CSSProperties;
+
+  return (
+    <div style={style} className={styles.flex}>
+      {children}
+    </div>
+  );
+};
