82bc013298182280d153b456d39a3fd75f65c1b8
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-02 14:48:14 +0100

    feat: update Home layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
-  return <div className={styles.body}>HOME PAGE</div>;
+  return (
+    <div className={styles.body}>
+      <div className={styles.navigation} role="navigation">
+        Nav
+      </div>
+      <div className={styles.main} role="main">
+        Main
+      </div>
+      <div className={styles.complementary} role="complementary">
+        Friend
+      </div>
+    </div>
+  );
 };
 
 export default Home;
