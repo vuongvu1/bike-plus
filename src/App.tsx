223045d024fe7dabf77c9698925dff17ced4a198
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-02 10:25:50 +0100

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Header } from "./components/Header";
+
 function App() {
-  return <h1>Bike Plus!</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Bike Plus!</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import styles from "./style.module.css";
+import Logo from "../../assets/bike-plus-logo.svg";
+import Avatar from "../../assets/sample-avatar.webp";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={Logo} alt="Bike Plus Logo" />
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="/">Community</a>
+          </li>
+          <li>
+            <a href="/marketplace">Marketplace</a>
+          </li>
+          <li>
+            <a href="/bike-repair">Bike Repair</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.avatar}>
+        <img src={Avatar} alt="User Avatar" />
+      </div>
+    </header>
+  );
+};
