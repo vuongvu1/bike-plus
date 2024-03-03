fb2ced2f666b60f7787deffafeb4c17300e41de6
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-03 17:05:15 +0100

    feat: add fake loading state to InputCard component

--- src/components/InputCard/InputCard.tsx
+++ src/components/InputCard/InputCard.tsx
@@ -4,11 +4,22 @@ import { CurrentUserAvatar } from "../CurrentUserAvatar";
 import { MockDataContext } from "../../contexts/MockDataContext";
 import { TextInput } from "../TextInput";
 import { IconButton } from "../IconButton";
+import { SkeletonCard } from "../SkeletonCard";
 import { Flex } from "../Flex";
 import { SendIcon } from "../../assets/icons/SendIcon";
 
 export const InputCard: React.FC = () => {
   const [inputValue, setInputValue] = React.useState("");
+  const [fakeLoading, setFakeLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const {
     addFeed,
     data: { user },
@@ -16,7 +27,14 @@ export const InputCard: React.FC = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (inputValue) {
+    if (!inputValue) {
+      return;
+    }
+    setFakeLoading(true);
+    setInputValue("");
+
+    setTimeout(() => {
+      setFakeLoading(false);
       addFeed({
         id: Math.random().toString(),
         content: inputValue,
@@ -24,26 +42,27 @@ export const InputCard: React.FC = () => {
         likes: 0,
         comments: 0,
       });
-      // createdAt: new Date().toISOString(),
-      setInputValue("");
-    }
+    }, 1000);
   };
 
   return (
-    <Card>
-      <CardContent>
-        <form onSubmit={handleSubmit}>
-          <Flex align="center" gap="var(--spacing-sm)">
-            <CurrentUserAvatar hideName />
-            <TextInput
-              value={inputValue}
-              onChange={setInputValue}
-              placeholder="What is on your mind?"
-            />
-            <IconButton icon={<SendIcon />} />
-          </Flex>
-        </form>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <Flex align="center" gap="var(--spacing-sm)">
+              <CurrentUserAvatar hideName />
+              <TextInput
+                value={inputValue}
+                onChange={setInputValue}
+                placeholder="What is on your mind?"
+              />
+              <IconButton icon={<SendIcon />} />
+            </Flex>
+          </form>
+        </CardContent>
+      </Card>
+      {fakeLoading && <SkeletonCard numOfLines={2} />}
+    </>
   );
 };
