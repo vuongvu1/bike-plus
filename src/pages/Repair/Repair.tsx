db9cbb16ab2b15f87cbaf702ef99a35670061abf
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-04 00:30:42 +0100

    feat: add map and styling to Repair page

--- src/pages/Repair/Repair.tsx
+++ src/pages/Repair/Repair.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from "./Repair.module.css";
+import { Typography } from "../../components/Typography";
 
 const Repair: React.FC = () => {
-  return <div className={styles.body}>Repair PAGE</div>;
+  return (
+    <div className={styles.body}>
+      <Typography variant="h1">
+        You can find the closest repair shops here:
+      </Typography>
+      <iframe
+        src="https://maps.google.com/maps?q=berlin&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        width="600"
+        height="450"
+        allowFullScreen
+        loading="lazy"
+      />
+    </div>
+  );
 };
 
 export default Repair;
