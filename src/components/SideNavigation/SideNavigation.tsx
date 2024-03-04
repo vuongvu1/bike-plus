c5cf289b9a4e03c2d7dc2dfa76742af2e2f0e26d
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-04 22:17:53 +0100

    feat: add EventIcon component and update SideNavigation

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { Flex } from "../Flex";
 import { Card, CardContent } from "../Card";
+import { FriendIcon } from "../../assets/icons/FriendIcon";
+import { GroupIcon } from "../../assets/icons/GroupIcon";
+import { EventIcon } from "../../assets/icons/EventIcon";
+import { BookmarkIcon } from "../../assets/icons/BookmarkIcon";
+
+const iconLabelMap = [
+  { label: "Find friends", icon: <FriendIcon /> },
+  { label: "Groups", icon: <GroupIcon /> },
+  { label: "Events", icon: <EventIcon /> },
+  { label: "Saved", icon: <BookmarkIcon /> },
+];
 
 export const SideNavigation: React.FC = () => (
   <Flex direction="column" gap="var(--spacing-sm)">
-    <Card>
-      <CardContent>Find a friend</CardContent>
-    </Card>
-    <Card>
-      <CardContent>Groups</CardContent>
-    </Card>
-    <Card>
-      <CardContent>Events</CardContent>
-    </Card>
-    <Card>
-      <CardContent>Saved</CardContent>
-    </Card>
+    {iconLabelMap.map(({ label, icon }) => (
+      <Card key={label}>
+        <CardContent>
+          <Flex align="center" gap="var(--spacing-sm)">
+            {icon} {label}
+          </Flex>
+        </CardContent>
+      </Card>
+    ))}
   </Flex>
 );
