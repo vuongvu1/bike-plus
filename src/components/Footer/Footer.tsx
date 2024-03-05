75798505020d40e5caa7009a3d0dce652318e873
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-05 22:45:45 +0100

    feat: update footer and header styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "./Footer.module.css";
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <Typography variant="p">
+      <Typography variant="span">
         © {new Date().getFullYear()} Hoang Vuong Vu
       </Typography>
     </footer>
