c904226f17f5d311b46cc5b9846af4626555111d
Vuong Vu <hoangvuong.v99@gmail.com>
2024-03-05 01:07:04 +0100

    feat: refactor NewFeeds component and add useScrollToEnd hook

--- src/components/NewFeeds/NewFeeds.tsx
+++ src/components/NewFeeds/NewFeeds.tsx
@@ -1,34 +1,115 @@
-import React from "react";
+import {
+  useEffect,
+  useState,
+  useRef,
+  FC,
+  useCallback,
+  useContext,
+  useMemo,
+} from "react";
 import { MockDataContext } from "../../contexts/MockDataContext";
+import { Card, CardContent } from "../Card";
+import { Flex } from "../Flex";
 import { FeedCard } from "../FeedCard";
 import { SkeletonCard } from "../SkeletonCard";
 import { useMockLoading } from "../../hooks/useMockLoading";
+import { useScrollToEnd } from "../../hooks/useScrollToEnd";
+import { debounce } from "../../utils/debounce";
+import type { FeedType } from "../../contexts/MockDataContext";
 
-export const NewFeeds: React.FC = () => {
+const FEED_STEP = 3;
+
+export const NewFeeds: FC = () => {
+  const [showingFeeds, setShowingFeeds] = useState<FeedType[]>([]);
+  const [isOutOfFeeds, setIsOutOfFeeds] = useState(false);
+  const feedRangeRef = useRef({ start: 0, end: FEED_STEP });
   const {
     deleteFeed,
-    data: { feeds, user: currentUser },
-  } = React.useContext(MockDataContext);
+    data: { feeds: allFeeds, user: currentUser },
+  } = useContext(MockDataContext);
 
-  const [isLoading] = useMockLoading();
-  const isNotReady = isLoading || !feeds;
+  const [isPageLoading] = useMockLoading();
+  const [isGettingNewFeeds, startGettingNewFeeds] = useMockLoading();
 
-  return isNotReady ? (
+  const isFeedsReady = !isPageLoading && allFeeds;
+
+  const increaseShowingFeeds = useCallback(() => {
+    const maxFeed = allFeeds.length;
+    const { start, end } = feedRangeRef.current;
+
+    // Reach the end of feeds
+    if (start === end && end === maxFeed) {
+      setIsOutOfFeeds(true);
+      return;
+    }
+
+    startGettingNewFeeds(() => {
+      // Append new feeds to showingFeeds
+      setShowingFeeds((currentFeeds) =>
+        currentFeeds.length > start
+          ? currentFeeds
+          : currentFeeds.concat(allFeeds.slice(start, end))
+      );
+
+      // Update feed range
+      feedRangeRef.current.start = feedRangeRef.current.end;
+      feedRangeRef.current.end += FEED_STEP;
+
+      if (feedRangeRef.current.end > maxFeed) {
+        feedRangeRef.current.end = maxFeed;
+      }
+    });
+  }, [allFeeds, startGettingNewFeeds]);
+
+  const debouncedSetShowingFeeds = useMemo(
+    () => debounce(increaseShowingFeeds, 500),
+    [increaseShowingFeeds]
+  );
+
+  useScrollToEnd(debouncedSetShowingFeeds);
+
+  // Set initial showingFeeds
+  useEffect(() => {
+    if (isFeedsReady) {
+      increaseShowingFeeds();
+    }
+  }, [isFeedsReady, increaseShowingFeeds]);
+
+  if (!isFeedsReady) {
+    return (
+      <>
+        <SkeletonCard />
+        <SkeletonCard numOfLines={2} />
+        <SkeletonCard numOfLines={4} />
+      </>
+    );
+  }
+
+  return (
     <>
-      <SkeletonCard />
-      <SkeletonCard numOfLines={2} />
-      <SkeletonCard numOfLines={4} />
-      <SkeletonCard numOfLines={2} />
+      {showingFeeds.map((feed) => (
+        <FeedCard
+          key={feed.id}
+          feed={feed}
+          onClose={
+            feed.user.id === currentUser.id ? () => deleteFeed(feed) : undefined
+          }
+        />
+      ))}
+      {isGettingNewFeeds && (
+        <>
+          <SkeletonCard />
+          <SkeletonCard numOfLines={2} />
+          <SkeletonCard numOfLines={4} />
+        </>
+      )}
+      {isOutOfFeeds && (
+        <Card>
+          <CardContent>
+            <Flex justify="center">You've caught up!</Flex>
+          </CardContent>
+        </Card>
+      )}
     </>
-  ) : (
-    feeds.map((feed) => (
-      <FeedCard
-        key={feed.id}
-        feed={feed}
-        onClose={
-          feed.user.id === currentUser.id ? () => deleteFeed(feed) : undefined
-        }
-      />
-    ))
   );
 };
